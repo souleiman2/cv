78105948ffb3d5d69429bea2534915292fa59cc9
Souleiman <souleiman_ayoub@hotmail.com>
2021-08-12 15:07:05 -0400

    started the backbone of the version 2

---
+++ src/app/Interfaces/project.ts
@@ -0,0 +1,5 @@
+export interface Project{
+    name : string,
+    image_url : string,
+    redirected_url : string
+}
---
+++ src/app/Services/projects-data.ts
@@ -0,0 +1,15 @@
+import { Project } from '../Interfaces/project'
+
+export const PROJECTS : Array<Project> = [
+    { name : "FastFix", image_url : "assets/tools.jpg", redirected_url : "https://souleiman2.github.io/FastFix/"},
+    { name : "McGillPhysHack : Angular velocity in arbitrary dimensions", image_url : "assets/multidimensionnal.jpg", redirected_url : "https://souleiman2.github.io/McGillPhysHack2018/"},
+    { name : "PyChessAI", image_url : "assets/chess.jpg", redirected_url : "https://github.com/souleiman2/ChessAI"},
+    { name : "Sudoku Generator", image_url : "assets/sudoku.png", redirected_url : "https://github.com/souleiman2/Sudoku-Generator"},
+    { name : "Unilinks", image_url : "assets/random_event.jpg", redirected_url : "https://unilinks-2cba6.web.app/app" },
+    { name : "Real estate listing", image_url : "assets/random_event.jpg", redirected_url : "https://ayoubsaade.github.io/maisons/"},
+    { name : "L-system", image_url : "assets/Lsystem.png", redirected_url : "https://souleiman2.github.io/L-system/"},
+    { name : "Hanoi Tower", image_url : "assets/sudoku.png", redirected_url : "https://github.com/souleiman2/Sudoku-Generator"},
+    { name : "Clone attack", image_url : "assets/tentacles.png", redirected_url : "https://github.com/LeKo0/TP2_ADC"},
+    { name : "Mender Sponge", image_url : "assets/mendersponge.jpg", redirected_url : "https://souleiman2.github.io/MenderSponge/main.html"},
+    { name : "Snake", image_url : "assets/snake.jpg", redirected_url : "https://souleiman2.github.io/snake/"}
+]
