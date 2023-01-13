87ff2314729c36b8fc324b1cf50adcceb0566f7b
souleiman2 <souleiman_ayoub@hotmail.com>
2023-01-12 19:23:09 -0500

    continued coding

---
+++ src/app/Interfaces/experience.ts
@@ -0,0 +1,11 @@
+export interface ITimelineItem {
+  year: string;
+  content: Experience[];
+}
+
+export interface Experience {
+  title: String;
+  subTitle: String | null;
+  elemList: String[] | null;
+  time: String | null;
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,21 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ToolbarComponent } from './Components/toolbar/toolbar.component';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { ExperienceComponent } from './Components/experience/experience.component';
+import { ExperienceItemComponent } from './Components/experience-item/experience-item.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ToolbarComponent
+    ToolbarComponent,
+    ExperienceComponent,
+    ExperienceItemComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FontAwesomeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
