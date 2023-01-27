e5e8db709ed137e01dc9676bd3d44cec0b5081ed
souleiman2 <souleiman_ayoub@hotmail.com>
2023-01-27 01:16:19 -0500

    added some project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -45,18 +45,40 @@ export class AppComponent {
 
   // Projects
   // - Featured Projects
+  
+
+
+
   featured_projects : Array<IProject> = [
     {
       title : "PyChessAI",
-      description : "This is the description",
-      dev_tools : ["Python"],
-      img_url : "assets/chess.jpg"
+      description : "Python Chess program where a user can choose to play with certain artificiel intelligences (machine learning and others) or play against another human",
+      dev_tools : ["Python", "Pygame"],
+      img_url : "assets/chess.jpg",
+      git_url : "https://github.com/souleiman2/ChessAI"
     },
     {
-      title : "PyChessAI2",
-      description : "This is the description",
-      dev_tools : ["Python"],
-      img_url : "assets/chess.jpg"
+      title : "McGill PhysHack",
+      description : "Visualisation of Angular velocity in arbitrary dimensions",
+      dev_tools : ["p5", "Javascript", "HTML"],
+      img_url : "assets/tesseract.jpg",
+      git_url : "https://github.com/souleiman2/McGillPhysHack2018",
+      demo_url : "https://souleiman2.github.io/McGillPhysHack2018/"
+    },
+    {
+      title : "NLP Practice",
+      description : "NLP and models to achieve questions answering and tagging. Used TF-IDF, Word2Vec, FastText and Bert.",
+      dev_tools : ["Jupyter Notebook", "Python", "torch", "transformers", "nltk", "sklearn", "scipy"],
+      img_url : "assets/word2vec.png",
+      git_url : "https://github.com/souleiman2/NLP-Practice"
+    },
+    {
+      title : "Rental Housing Viz",
+      description : "Montreal Rental Real Estate Data Visualization",
+      dev_tools : ["d3js", "Javascript", "HTML"],
+      img_url : "assets/housing.jpg",
+      git_url : "https://github.com/souleiman2/Logement-Locatif-Viz",
+      demo_url : "https://souleiman2.github.io/Logement-Locatif-Viz/"
     }
     /**
      * McGill
